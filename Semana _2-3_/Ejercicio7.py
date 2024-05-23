f1bdb73f98923bb1597162d35c155262faff61ee
jo-C17 <joseluiscondezocrack17@gmail.com>
2024-05-22 22:43:36 -0500

    Ejerccio 7 solucion

--- Semana _2-3_/Ejercicio7.py
+++ Semana _2-3_/Ejercicio7.py
@@ -1,8 +1,9 @@
 '''Escribir un programa que muestre por pantalla la tabla de multiplicar del
 1 al 10.'''
 
-num=10
+for numero in range(1,11):
+    print(f"La tabla del {numero}")
 
-for i in range(1,num+1):
-    mult=i*num
-    print (mult)
+    for i in range(1,11):
+        mult=numero*i
+        print(f"{numero} x {i} = {mult}")
